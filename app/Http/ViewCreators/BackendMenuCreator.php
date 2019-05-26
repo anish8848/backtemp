782b0ba8b2ebbd6cd77f54c8e88ade078cb63a6b
anish <anish@accessworld.net>
2019-05-26 17:20:55 +0545

    first commit

---
+++ app/Http/ViewCreators/BackendMenuCreator.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\ViewCreators;
+
+use Illuminate\View\View;
+
+class BackendMenuCreator
+{
+
+    /**
+     * The user model.
+     *
+     * @var \App\User;
+     */
+    protected $user;
+
+    /**
+     * Create a new menu bar composer.
+     */
+    public function __construct()
+    {
+        $this->user = auth()->user();
+    }
+
+    /**
+     * Bind data to the view.
+     *
+     * @param  View $view
+     * @return void
+     */
+    public function create(View $view)
+    {
+
+
+        $view->with('allMenu', $menu);
+    }
+}
